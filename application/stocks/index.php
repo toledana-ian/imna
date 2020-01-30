fc421eecfe4ad125279cdf7366f8744943945215
Christian Toledana <c.toledana@aidea.com.ph>
2020-01-30 12:12:59 +0800

    Test for php server

--- application/stocks/index.php
+++ application/stocks/index.php
@@ -100,41 +100,17 @@
 					</tr>
 					</thead>
 					<tbody>
-					<tr>
-						<td>Tiger Nixon</td>
-						<td>System Architect</td>
-						<td>Edinburgh</td>
-						<td>61</td>
-						<td>2011/04/25</td>
-						<td>$320,800</td>
-					</tr>
-					<tr>
-						<td>Garrett Winters</td>
-						<td>Accountant</td>
-						<td>Tokyo</td>
-						<td>63</td>
-						<td>2011/07/25</td>
-						<td>$170,750</td>
-					</tr>
+					<?php for($i=0; $i<100; $i++) { ?>
 					<tr>
 						<td>Ashton Cox</td>
 						<td>Junior Technical Author</td>
 						<td>San Francisco</td>
 						<td>66</td>
 						<td>2009/01/12</td>
-						<td>$86,000</td>
+						<td>0</td>
 					</tr>
+					<?php } ?>
 					</tbody>
-					<tfoot>
-					<tr>
-						<th>Name</th>
-						<th>Position</th>
-						<th>Office</th>
-						<th>Age</th>
-						<th>Start date</th>
-						<th>Salary</th>
-					</tr>
-					</tfoot>
 				</table>
 			</div>
 		</div>
