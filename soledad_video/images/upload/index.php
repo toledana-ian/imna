c2164d4908925f545b363749858d5a8f842e32f3
Christian Toledana <toledana.ian@gmail.com>
2020-02-03 14:54:27 +0800

    Remove unnecessary files

--- soledad_video/images/upload/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
-    $filename = $_SERVER['REMOTE_ADDR'].'_'.(new DateTime())->format('Y-m-d H-i-s').'.jpg';
-    $myfile = fopen($filename, "w") or die("Unable to open file!");
-    fwrite($myfile, base64_decode($_REQUEST['file']));
-    fclose($myfile);
-}
-?>
